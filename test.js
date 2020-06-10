ccef4be88d9004188f88875f79979319a49b9ff4
Samuel <s.a.j.oosterholt@student.tue.nl>
2020-06-10 16:17:01 +0200

    Formatting and keywords.

--- test.js
+++ test.js
@@ -11,19 +11,31 @@ describe("srol", function () {
     it('should export a function', function () {
         assert(typeof srol === "function");
     });
+
     it('should thrown an error for a non positive length', function () {
-        assert.throws(() => { new srol() }, TypeError);
-        assert.throws(() => { new srol(-1) }, TypeError);
+        assert.throws(() => {
+            new srol()
+        }, TypeError);
+        assert.throws(() => {
+            new srol(-1)
+        }, TypeError);
     });
+
     it('should start off with an average of 0', function () {
         let rollingAverage = new srol(1);
         assert(rollingAverage.average() === 0);
     });
+
     it('should only accept numbers', function () {
         let rollingAverage = new srol(1);
-        assert.throws(() => { rollingAverage.add("1") }, TypeError);
-        assert.throws(() => { rollingAverage.add(["yes"]) }, TypeError);
+        assert.throws(() => {
+            rollingAverage.add("1")
+        }, TypeError);
+        assert.throws(() => {
+            rollingAverage.add(["yes"])
+        }, TypeError);
     });
+
     // [1.5, 4.5] has an average of 3.
     it('should return the correct average for a non full array', function () {
         let rollingAverage = new srol(3);
@@ -31,6 +43,7 @@ describe("srol", function () {
         assert(!rollingAverage.filled);
         assert(rollingAverage.average() === 3);
     });
+
     // [1.5, 4.5, 6] has an average of 4.
     it('should return the correct average for a full array', function () {
         let rollingAverage = new srol(3);
