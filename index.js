e73a7b93183c54ca35d49965157850b95bf95562
Samuel <s.a.j.oosterholt@student.tue.nl>
2020-06-13 13:47:50 +0200

    Applied standard Eslint.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict'
 
 /**
  * Creates a RollingAverage class with a memory size of {@param length}.
@@ -8,13 +8,13 @@
  * @param {number} length - Size of the internal array.
  * @constructor
  */
-function RollingAverage(length) {
-    if (length <= 0 || arguments.length === 0) throw TypeError("Expected non negative length.");
-    this.sum = 0;
-    this.length = length;
-    this.filled = false;
-    this.pointer = 0;
-    this.array = [];
+function RollingAverage (length) {
+  if (length <= 0 || arguments.length === 0) throw TypeError('Expected non negative length.')
+  this.sum = 0
+  this.length = length
+  this.filled = false
+  this.pointer = 0
+  this.array = []
 }
 
 /**
@@ -22,16 +22,16 @@ function RollingAverage(length) {
  * @param {number | [number]} a - Items to add to the RollingAverage array.
  */
 RollingAverage.prototype.add = function (a) {
-    if (typeof (a) === "number") {
-        this._addNumber(a);
-    } else if (Array.isArray(a)) {
-        for (let i = 0; i < a.length; i++) {
-            this._addNumber(a[i]);
-        }
-    } else {
-        throw TypeError("Unsupported argument type " + typeof (a));
+  if (typeof (a) === 'number') {
+    this._addNumber(a)
+  } else if (Array.isArray(a)) {
+    for (let i = 0; i < a.length; i++) {
+      this._addNumber(a[i])
     }
-};
+  } else {
+    throw TypeError('Unsupported argument type ' + typeof (a))
+  }
+}
 
 /**
  * Private function that adds a single number to the array.
@@ -40,23 +40,23 @@ RollingAverage.prototype.add = function (a) {
  * @private
  */
 RollingAverage.prototype._addNumber = function (a) {
-    if (isNaN(a)) {
-        throw TypeError("expected first argument to be an number");
-    }
-    if (this.array[this.pointer] !== undefined) {
-        this.sum -= this.array[this.pointer];
-    }
+  if (isNaN(a)) {
+    throw TypeError('expected first argument to be an number')
+  }
+  if (this.array[this.pointer] !== undefined) {
+    this.sum -= this.array[this.pointer]
+  }
 
-    this.array[this.pointer] = a;
-    this.sum += a;
+  this.array[this.pointer] = a
+  this.sum += a
 
-    if (this.pointer + 1 >= this.length) {
-        this.pointer = 0;
-        this.filled = true;
-    } else {
-        this.pointer++;
-    }
-};
+  if (this.pointer + 1 >= this.length) {
+    this.pointer = 0
+    this.filled = true
+  } else {
+    this.pointer++
+  }
+}
 
 /**
  * Returns the average of the array.
@@ -66,11 +66,11 @@ RollingAverage.prototype._addNumber = function (a) {
  * @returns {number} - The average of the array.
  */
 RollingAverage.prototype.average = function () {
-    return this.sum / (this.filled || this.pointer === 0 ? this.length : this.pointer);
-};
+  return this.sum / (this.filled || this.pointer === 0 ? this.length : this.pointer)
+}
 
 /**
  * Exposes the class.
  * @type {Function}
  */
-module.exports = RollingAverage;
+module.exports = RollingAverage
--- test.js
+++ test.js
@@ -1,64 +1,66 @@
-"use strict";
+'use strict'
 
-require("mocha");
-let assert = require("assert");
-let srol = require("./")
+require('mocha')
+const assert = require('assert')
+const Srol = require('./')
 
 /**
- * Tests srol class functions and properties.
+ * Tests Srol class functions and properties.
  */
-describe("srol", function () {
-    it('should export a function', function () {
-        assert(typeof srol === "function");
-    });
+describe('Srol', function () {
+  it('should export a function', function () {
+    assert(typeof Srol === 'function')
+  })
 
-    it('should thrown an error for a non positive length', function () {
-        assert.throws(() => {
-            new srol()
-        }, TypeError);
-        assert.throws(() => {
-            new srol(-1)
-        }, TypeError);
-    });
+  it('should thrown an error for a non positive length', function () {
+    let test = null
+    assert.throws(() => {
+      test = new Srol()
+    }, TypeError)
+    assert.throws(() => {
+      test = new Srol(-1)
+    }, TypeError)
+    assert(test === null)
+  })
 
-    it('should start off with an average of 0', function () {
-        let rollingAverage = new srol(1);
-        assert(rollingAverage.average() === 0);
-    });
+  it('should start off with an average of 0', function () {
+    const rollingAverage = new Srol(1)
+    assert(rollingAverage.average() === 0)
+  })
 
-    it('should only accept numbers', function () {
-        let rollingAverage = new srol(1);
-        assert.throws(() => {
-            rollingAverage.add("1")
-        }, TypeError);
-        assert.throws(() => {
-            rollingAverage.add(["yes"])
-        }, TypeError);
-    });
+  it('should only accept numbers', function () {
+    const rollingAverage = new Srol(1)
+    assert.throws(() => {
+      rollingAverage.add('1')
+    }, TypeError)
+    assert.throws(() => {
+      rollingAverage.add(['yes'])
+    }, TypeError)
+  })
 
-    // [1.5, 4.5] has an average of 3.
-    it('should return the correct average for a non full array', function () {
-        let rollingAverage = new srol(3);
-        rollingAverage.add([1.5, 4.5]);
-        assert(!rollingAverage.filled);
-        assert(rollingAverage.average() === 3);
-    });
+  // [1.5, 4.5] has an average of 3.
+  it('should return the correct average for a non full array', function () {
+    const rollingAverage = new Srol(3)
+    rollingAverage.add([1.5, 4.5])
+    assert(!rollingAverage.filled)
+    assert(rollingAverage.average() === 3)
+  })
 
-    // [1.5, 4.5, 6] has an average of 4.
-    it('should return the correct average for a full array', function () {
-        let rollingAverage = new srol(3);
-        rollingAverage.add([1.5, 4.5]);
-        rollingAverage.add(6);
-        assert(rollingAverage.filled);
-        assert(rollingAverage.average() === 4);
-    });
+  // [1.5, 4.5, 6] has an average of 4.
+  it('should return the correct average for a full array', function () {
+    const rollingAverage = new Srol(3)
+    rollingAverage.add([1.5, 4.5])
+    rollingAverage.add(6)
+    assert(rollingAverage.filled)
+    assert(rollingAverage.average() === 4)
+  })
 
-    // size 3: [1.5, 4.5, 6].add(4.5) -> [4.5, 4.5, 6] has an average of 4.5
-    it('should correctly apply FIFO', function () {
-        let rollingAverage = new srol(3);
-        rollingAverage.add([1.5, 4.5, 6]);
-        rollingAverage.add(4.5);
-        assert(rollingAverage.filled);
-        assert(rollingAverage.average() === 5);
-    });
-});
+  // size 3: [1.5, 4.5, 6].add(4.5) -> [4.5, 4.5, 6] has an average of 4.5
+  it('should correctly apply FIFO', function () {
+    const rollingAverage = new Srol(3)
+    rollingAverage.add([1.5, 4.5, 6])
+    rollingAverage.add(4.5)
+    assert(rollingAverage.filled)
+    assert(rollingAverage.average() === 5)
+  })
+})
